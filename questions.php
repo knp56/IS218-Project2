4ccc24bd24fa704e639d59bc531e70b2db60cebb
knp56 <51336521+knp56@users.noreply.github.com>
2019-11-26 18:12:20 -0500

    Create questions.php

---
+++ questions.php
@@ -0,0 +1,89 @@
+<?php
+
+require("pdo.php");
+
+$userID=filter_input(INPUT_GET,'userID');
+$questionName=filter_input(INPUT_POST,'questionName');
+$questionBody=filter_input(INPUT_POST,'questionBody');
+$questionSkills=filter_input(INPUT_POST,'questionSkills');
+$doubleCheckSkills=explode(',',$questionSkills);
+
+
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+{
+    $Validate=true;
+    if (empty($questionName))
+    {
+        $NameError = "Must provide a Question Name.";
+        $Validate=false;
+    }
+    elseif(strlen($questionName) <3)
+    {
+        $NameError= "Question Name must be 3 characters or more.";
+        $Validate=false;
+    }
+    if (empty($questionBody))
+    {
+        $bodyError = "Must provide a Question Body.";
+        $Validate=false;
+    }
+    elseif(strlen($questionBody) >= 500)
+    {
+        $bodyError= "Question Body cannot be more than 500 characters.";
+        $Validate=false;
+    }
+    if (empty($questionSkills))
+    {
+        $skillError = "Must provide skills.";
+        $Validate=false;
+    }
+    elseif(count($doubleCheckSkills)<2)
+    {
+        $skillError= "Must provide 2 or more skills.";
+        $Validate=false;
+    }
+
+
+    if($Validate==true)
+    {
+        $query = 'INSERT INTO questions
+                    (ownerID,title,body,skills)
+                VALUES
+                (:ownerID,:title,:body,:skills)';
+
+        $statement = $db->prepare($query);
+
+        $statement->bindValue('ownerID',$userID);
+        $statement->bindValue('questionName',$questionName);
+        $statement->bindValue('questionBody',$questionBody);
+        $statement->bindValue('questionSkills',$questionSkills);
+
+        $statement->execute();
+        $statement->closeCursor();
+
+
+    }
+
+}
+?>
+
+
+<html lang="en">
+
+<head><title> Question Form</title></head>
+
+<h1> Questionnaire </h1>
+<div>
+    Question Name = <?php if(!$NameError) echo $questionName; ?>
+    <span <span class="error"><?php echo $NameError; ?></span>
+</div>
+<div>
+    Question Body = <?php if(!$bodyError) echo $questionBody; ?>
+    <span <span class="error"><?php echo $bodyError; ?></span>
+</div>
+<div>
+    Question Skills = <?php if (!$skillError) echo $questionSkills; ?>
+    <span <span class="error"><?php echo $skillError; ?></span>
+</div>
+
+</html>
