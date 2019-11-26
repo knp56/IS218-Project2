80b8c7d1ce20cdeb107d020cf82877b79f6d18ae
knp56 <51336521+knp56@users.noreply.github.com>
2019-11-26 18:12:46 -0500

    Create pdo.php

---
+++ pdo.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>PDO demo!</h1>";
+$username = 'bcn3_proj';
+$password = 'w9hLvRFP';
+$hostname = 'sql1.njit.edu';
+$dsn = "mysql:host=$hostname;dbname=$username";
+try {
+    $db = new PDO($dsn, $username, $password);
+    echo "Connected successfully<br>";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
