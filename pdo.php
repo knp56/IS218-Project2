a4fd88016ff1d35a2ad92383c6eebf961af18c48
knp56 <51336521+knp56@users.noreply.github.com>
2019-11-28 19:45:13 -0500

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -7,9 +7,9 @@ $hostname = 'sql1.njit.edu';
 $dsn = "mysql:host=$hostname;dbname=$username";
 try {
     $db = new PDO($dsn, $username, $password);
-    echo "Connected successfully<br>";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 
 ?>
+
